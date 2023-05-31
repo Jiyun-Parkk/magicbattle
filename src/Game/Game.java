b64ab9e57d3ef17f1a98d84e21bd54db88edc4a7
alsth712 <alsth712@naver.com>
2023-05-31 17:51:04 +0900

    feat : 경기 기능 구현

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,33 +1,89 @@
 package Game;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 import wizard.Wizard;
 
 public class Game {
-
-  // 승자 패
-  public void Game(ArrayList<Wizard> list) {
-
-    // 마법사가 두명인지 확인한다.
-
-
-  }
+    Wizard player1;
+    Wizard player2;
+    int firstAttack;
+    Wizard winner;
 
 
+    public Game(ArrayList<Wizard> listOfPlayers) {
+        if (listOfPlayers.size() == 2) {
+            System.out.println("참가자가 2명이니 경기를 시작하겠습니다.");
+            this.player1 = listOfPlayers.get(0);
+            this.player2 = listOfPlayers.get(1);
+            System.out.println("========================================");
+            System.out.println("현재 상황 : " + player1.getName() + "의 체력: " + listOfPlayers.get(1).getHp());
+            System.out.println("현재 상황 : " + player2.getName() + "의 체력: " + listOfPlayers.get(0).getHp());
+            System.out.println("========================================");
+        } else {
+            System.out.println("참가자를 더 생성 해 주세요.");
+        }
+        deathMatch(listOfPlayers);
+    }
+
+    // 승자 패
 
 
-  // TODO 선공자를 정한다. (method)
+    // TODO 선공자를 정한다. (method)
+    public void setFirstAttacker() {
+        Random whoFirst = new Random();
+        this.firstAttack = whoFirst.nextInt(2);
+    }
 
-  // TODO 경기 하는 메서드 서로 공격.. (wizard list, 선공자 인덱스)
-  // 선공자 부터 공격 attack
-  // 한명이 체력이 다하면 쥬금 끝
+    // TODO 경기 하는 메서드 서로 공격.. (wizard list, 선공자 인덱스)
+    public void deathMatch(ArrayList<Wizard> listOfPlayers) {
 
-  // 공격자 = 1
-  // while (list.get(0).hp > 0 && player.get(1).hp > 0)
-  // 공격!
-  // 공격자 = 공격자 == 1 ? 2 : 1
+        setFirstAttacker();
 
-  // TODO 승자패자 getter  (method)
+        while (listOfPlayers.get(0).getHp() > 0 && listOfPlayers.get(1).getHp() > 0) {
+            Wizard victim;
+            if (firstAttack == 0) {
+                victim = player2;
+            } else {
+                victim = player1;
+            }
+            System.out.println("선공 인덱스는 : " + firstAttack);
 
+            listOfPlayers.get(firstAttack).attack(victim);
+            System.out.println("========================================");
+            System.out.println("현재 상황 : " + player1.getName() + "의 체력: " + listOfPlayers.get(1).getHp());
+            System.out.println("현재 상황 : " + player2.getName() + "의 체력: " + listOfPlayers.get(0).getHp());
+            System.out.println("========================================");
+            if (firstAttack == 0) {
+                System.out.println("0일떄");
+                firstAttack = 1;
+            } else {
+                System.out.println("1일때");
+                firstAttack = 0;
+            }
+        }
+        if (player1.getHp() <= 0) {
+            winner = player2;
+        } else {
+            winner = player1;
+        }
+        endGame();
+    }
+
+    // 선공자 부터 공격 attack
+    // 한명이 체력이 다하면 쥬금 끝
+
+    // 공격자 = 1
+    // while (list.get(0).hp > 0 && player.get(1).hp > 0)
+
+    // 공격!
+    // 공격자 = 공격자 == 1 ? 2 : 1
+
+    // TODO 승자패자 getter  (method)
+    public void endGame() {
+        System.out.println("경기가 끝났습니다.");
+        System.out.println("승자는: " + winner);
+    }
 
 }
--- src/wizard/Wizard.java
+++ src/wizard/Wizard.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import magic.Magic;
 
@@ -16,49 +17,49 @@ import magic.Magic;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Wizard {
 
-	private int hp;
-	private int mp;
-	private int age;
-	private String name;
+    private int hp;
+    private int mp;
+    private int age;
+    private String name;
 
-	public Wizard(ArrayList<Wizard> list) {
-		Random random = new Random();
-		hp = random.nextInt(100) + 1;
-		mp = random.nextInt(100) + 1;
-		String orderText = "첫번째";
-		if (list.size() == 1) {
-			orderText = "두번째";
-		}
-		Scanner sc = new Scanner(System.in);
-		System.out.printf("%s 마법사의 이름을 입력해주세요. : ", orderText);
-		name = sc.nextLine();
-		System.out.printf("%s 마법사의 나이를 입력해주세요. : ", orderText);
-		age = sc.nextInt();
-		System.out.println("========================================");
-	}
+    public Wizard(ArrayList<Wizard> list) {
+        Random random = new Random();
+        hp = random.nextInt(100) + 1;
+        mp = random.nextInt(100) + 1;
+        String orderText = "첫번째";
+        if (list.size() == 1) {
+            orderText = "두번째";
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%s 마법사의 이름을 입력해주세요. : ", orderText);
+        name = sc.nextLine();
+        System.out.printf("%s 마법사의 나이를 입력해주세요. : ", orderText);
+        age = sc.nextInt();
+        System.out.println("========================================");
+    }
 
-	public void attack(Wizard victim) {
+    public void attack(Wizard victim) {
+        Magic[] listOfMagic = {new Magic("파이어볼", 20), new Magic("아이스볼", 10), new Magic("썬더볼", 30),
+                new Magic("회오리바람~~", 5)};
+        int random = new Random().nextInt(listOfMagic.length);
+        Magic selectedMagic = listOfMagic[random];
+        int victimHP = victim.getHp();
+        int computeHP = victimHP - selectedMagic.computedPower(age);
+        victim.setHp(computeHP);
 
-		Magic[] listOfMagic = {new Magic("파이어볼", 20), new Magic("아이스볼", 10), new Magic("썬더볼", 30),
-				new Magic("회오리바람~~", 5)};
-		int random = new Random().nextInt(listOfMagic.length);
-		Magic selectedMagic = listOfMagic[random];
-		int victimHP = victim.getHp();
-		int computeHP = victimHP - selectedMagic.computedPower(age);
-		victim.setHp(computeHP);
+        System.out.printf("%s(이)가 %s(을)를 %s(으)로 공격했습니다. %s공격력 %d%n", name, victim.getName(),
+                selectedMagic.getMagicName(), selectedMagic.getMagicName(), selectedMagic.getMagicPower());
+        selectedMagic.printEffectResult(age);
+        System.out.printf("%s(이)가 데미지 %d을 받았습니다.%n", victim.getName(),
+                selectedMagic.computedPower(age));
+    }
 
-		System.out.printf("%s(이)가 %s(을)를 %s(으)로 공격했습니다. %s공격력 %d%n", name, victim.getName(),
-				selectedMagic.getMagicName(), selectedMagic.getMagicName(), selectedMagic.getMagicPower());
-		selectedMagic.printEffectResult(age);
-		System.out.printf("%s(이)가 데미지 %d을 받았습니다.%n", victim.getName(),
-				selectedMagic.computedPower(age));
-	}
-
-	@Override
-	public String toString() {
-		return String.format("name: %s, age: %d", name, age);
-	}
+    @Override
+    public String toString() {
+        return String.format("name: %s, age: %d", name, age);
+    }
 
 }
