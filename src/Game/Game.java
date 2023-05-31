56edef0f1f183d1b8ab3463d26ba5f3bbdc02662
alsth712 <alsth712@naver.com>
2023-05-31 18:06:02 +0900

    fix: 승자 관련 에러 수정

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -41,24 +41,21 @@ public class Game {
 
         setFirstAttacker();
 
-        while (listOfPlayers.get(0).getHp() > 0 && listOfPlayers.get(1).getHp() > 0) {
+        while (player1.getHp() > 0 && player2.getHp() > 0) {
             Wizard victim;
             if (firstAttack == 0) {
                 victim = player2;
             } else {
                 victim = player1;
             }
-
             listOfPlayers.get(firstAttack).attack(victim);
             System.out.println("========================================");
-            System.out.println("현재 상황 : " + player1.getName() + "의 체력: " + listOfPlayers.get(1).getHp());
-            System.out.println("현재 상황 : " + player2.getName() + "의 체력: " + listOfPlayers.get(0).getHp());
+            System.out.println("현재 상황 : " + player1.getName() + "의 체력: " + player1.getHp());
+            System.out.println("현재 상황 : " + player2.getName() + "의 체력: " + player2.getHp());
             System.out.println("========================================");
             if (firstAttack == 0) {
-                System.out.println("0일떄");
                 firstAttack = 1;
             } else {
-                System.out.println("1일때");
                 firstAttack = 0;
             }
         }
@@ -70,18 +67,10 @@ public class Game {
         endGame();
     }
 
-    // 선공자 부터 공격 attack
-    // 한명이 체력이 다하면 쥬금 끝
-
-    // 공격자 = 1
-    // while (list.get(0).hp > 0 && player.get(1).hp > 0)
-
-    // 공격!
-    // 공격자 = 공격자 == 1 ? 2 : 1
-
     // TODO 승자패자 getter  (method)
     public void endGame() {
         System.out.println("경기가 끝났습니다.");
+        System.out.println("축하드립니다!!!!");
         System.out.println("승자는: " + winner);
     }
 
